9922aded7c43ce80d9a1686aa27dfc1e9b241e12
Escobar1945 <candaroglu197878@gmail.com>
2022-11-13 16:03:48 +0300

    Merge tag 'android-13.0.0_r13' of https://android.googlesource.com/pl… …atform/build/soong into HEAD
    Android 13.0.0 release 13

--- android/sdk.go
+++ android/sdk.go
@@ -660,6 +660,10 @@ type SdkMemberType interface {
 	// RequiresBpProperty returns true if this member type requires its property to be usable within
 	// an Android.bp file.
 	RequiresBpProperty() bool
+	
+	// SupportedBuildReleases returns the string representation of a set of target build releases that
+	// support this member type.
+	SupportedBuildReleases() string
 
 	// UsableWithSdkAndSdkSnapshot returns true if the member type supports the sdk/sdk_snapshot,
 	// false otherwise.
@@ -759,6 +763,11 @@ type SdkMemberTypeBase struct {
 	// When set to true BpPropertyNotRequired indicates that the member type does not require the
 	// property to be specifiable in an Android.bp file.
 	BpPropertyNotRequired bool
+	
+	// The name of the first targeted build release.
+	//
+	// If not specified then it is assumed to be available on all targeted build releases.
+	SupportedBuildReleaseSpecification string
 
 	SupportsSdk     bool
 	HostOsDependent bool
@@ -778,6 +787,10 @@ func (b *SdkMemberTypeBase) SdkPropertyName() string {
 
 func (b *SdkMemberTypeBase) RequiresBpProperty() bool {
 	return !b.BpPropertyNotRequired
+}
+
+func (b *SdkMemberTypeBase) SupportedBuildReleases() string {
+	return b.SupportedBuildReleaseSpecification
 }
 
 func (b *SdkMemberTypeBase) UsableWithSdkAndSdkSnapshot() bool {
@@ -933,6 +946,10 @@ type SdkMemberContext interface {
 
 	// RequiresTrait returns true if this member is expected to provide the specified trait.
 	RequiresTrait(trait SdkMemberTrait) bool
+	
+	// IsTargetBuildBeforeTiramisu return true if the target build release for which this snapshot is
+	// being generated is before Tiramisu, i.e. S.
+	IsTargetBuildBeforeTiramisu() bool
 }
 
 // ExportedComponentsInfo contains information about the components that this module exports to an
